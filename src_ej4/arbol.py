2542991b14b9ecb0e58e155052adc1e12d9954c7
Nicolás Berríos Verboven <nbverboven@gmail.com>
2016-06-07 15:10:47 -0300

    Agregados algunos métodos del ej4

--- src_ej4/arbol.py
+++ src_ej4/arbol.py
@@ -1,7 +1,9 @@
 class Arbol(object):
     def __init__(self, elemento=None, izquierda=None, derecha=None):
         self.r = elemento
+        self._vacio = True
         if elemento is not None:
+            self._vacio = False
             if izquierda:
                 self._izquierda = izquierda
             else:
@@ -13,3 +15,35 @@ class Arbol(object):
         else:
             if izquierda is not None or derecha is not None:
                 raise TypeError("arbol mal formado")
+
+    def vacio(self):
+        return self._vacio
+
+    def raiz(self):
+        try:
+            return self.r
+        except AttributeError:
+            raise AttributeError("El árbol está vacío")
+
+    def izquierda(self):
+        if self._iquierda is not None:
+            return self._izquierda
+        else:
+            raise AttributeError("El árbol no tiene rama izquierda")
+
+    def derecha(self):
+        try:
+            return self._derecha
+        except AttributeError:
+            raise AttributeError("El árbol no tiene rama derecha")
+
+    def __eq__(self, otro_arbol):
+        if self._vacio and otro_arbol.vacio():
+            return True
+        elif (self._vacio and not otro_arbol.vacio()) or (not self._vacio and otro_arbol.vacio()):
+            return False
+        else:
+            return self.r == otro_arbol.raiz() and self._izquierda.__eq__(otro_arbol.izquierda()) and self._derecha.__eq__(otro_arbol.derecha())
+
+    def find(self, a):
+        return (not self._vacio) and (self.r == a or (self._izquierda.find(a)) or (self._derecha.find(a)))
