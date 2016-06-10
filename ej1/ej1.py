c9cebd1203925b05a0662977d89db5c17cedb546
guidosalustri <gsalustri@ws6.labo4>
2016-06-10 10:22:22 -0300

    distancias de listas arreglado

--- ej1/ej1.py
+++ ej1/ej1.py
@@ -15,14 +15,14 @@ def listaDePuntos(archi):
 	f.close()
 	return lista
 
-# print(lista)
+#print(listaDePuntos(archi))
 
 def distancia(punto1, punto2):
 	return sqrt( ( abs(punto1[0]-punto2[0]) )**2 + ( abs(punto1[1]-punto2[1]) )**2 )
 
 def listaDeDist(a):
-	if len(a) == 1:
-		return [0.0]
+	if len(a) == 0:
+		return []
 	else:	
 		l = []
 		i = 1
@@ -34,7 +34,8 @@ def listaDeDist(a):
 # print(listaDeDist(lista))
 
 def distanciaMinima(a):
-	if len(a) == 1:
+	if len(a)==0: return None
+	elif len(a) == 1:
 		return a[0]
 	else:	
 		mini = distanciaMinima(a[1:])
